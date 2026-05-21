b2df0a665c3fc831559a5c71961bc717ff825cb6
stort040 <stort040@umn.edu>
2026-05-21 12:35:55 -0500

    Give database files a proper name

--- frontend/windtunnel-site/app/api/rpm/route.ts
+++ frontend/windtunnel-site/app/api/rpm/route.ts
@@ -1,14 +1,24 @@
 import Database from "better-sqlite3";
 
+// The purpose of this file is to retrieve the average RPM query from the database.
+// SQL requests are dataprone, so error catching is important.
+
 export async function GET() {
   try {
-    const db = new Database("example_table.db");
-    const row = db
-      .prepare("SELECT AVG(rpm) as average FROM sql_example")
-      .get();
+    // Access local database
+    const db = new Database("windtunnel_sensors.db");
+
+    // Store data in a variable
+    const average_rpm = db
+      .prepare("SELECT AVG(rpm) as average FROM motor_rpm") // prompt database with SQL query
+      .get();                                               // receieve data 
     db.close();
     
-    return Response.json({ average: row?.average ?? 0 });
+    // Return data as a JSON
+    // Error checking...
+    // (average_rpm?.average) only returns if average_rpm is not null
+    // (0) returns if average_rpm doesn't exist
+    return Response.json({ average: average_rpm?.average ?? 0 });
   } 
   
   catch (error) {
--- frontend/windtunnel-site/app/page.tsx
+++ frontend/windtunnel-site/app/page.tsx
@@ -1,14 +1,17 @@
+/* eslint-disable react-hooks/error-boundaries */
 export default async function Home() {
   try {
-    const res = await fetch("http://localhost:3000/api/rpm");
-    const data = await res.json();
+    const res = await fetch("http://localhost:3000/api/rpm"); // database response generated from api/rpm route
+    const data = await res.json(); // database response as a JSON
 
     return (
       <main>
         <h1>Average RPM</h1>
+        {/* ternary if/else operation to error check database API response */}
         <p>{data.error ? `Error: ${data.error}` : Math.round(data.average)}</p>
       </main>
     );
+
   } catch (error) {
     return (
       <main>
