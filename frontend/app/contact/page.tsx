8a2d6fd87a9ec284e09735e39fb91d0291907d6e
MagmaC4 <magmac495@gmail.com>
2026-08-10 12:38:06 -0500

    Add text to contact page?

--- frontend/app/contact/page.tsx
+++ frontend/app/contact/page.tsx
@@ -1,2 +1,15 @@
+// className="flex items-center justify-center min-h-dvh"
+
 export default function Page(){
-    return(<p> contact </p>)}
+    return(
+        <div>
+            <p className="text-center m-8">
+                Hello!
+                <br/>This website was created by <a className="underline" target="_blank" href="https://www.linkedin.com/in/benjamin-stortroen-b61400347/">Benjamin Stortroen</a> at the Aerospace Engineering and Mechanics front desk.
+                <br/> It serves as a live dashboard of the open return wind tunnel in Akerman 430.
+                <br/> The github for the entire project can be found <a className="underline" target="_blank" href="https://github.com/MagmaC4/windtunnel">here.</a>
+                <br/> If any issues occur please email me at stort040@umn.edu
+            </p>
+        </div>
+    )
+}
