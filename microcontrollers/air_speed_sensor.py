d4d212e3395714ea58f90fe07a79818206db3fd5
MagmaC4 <magmac495@gmail.com>
2026-08-12 14:47:08 -0500

    Add airspeed sensor program

---
+++ microcontrollers/air_speed_sensor.py
@@ -0,0 +1,23 @@
+import time
+import board
+
+import adafruit_pcf8591.pcf8591 as PCF
+from adafruit_pcf8591.analog_in import AnalogIn
+from adafruit_pcf8591.analog_out import AnalogOut
+
+i2c = board.I2C()
+pcf = PCF.PCF8591(i2c)
+
+pcf_in_0 = AnalogIn(pcf, PCF.A0)
+
+
+while True:
+    raw_value = pcf_in_0.value
+    scaled_value = (raw_value / 65535) * pcf_in_0.reference_voltage
+    air_speed = 18.9 * (scaled_value ** 0.484)
+    print("Pin 0: %0.5fV" % (scaled_value))
+    print("Air Speed: %0.5fV" % (air_speed))
+    print("")
+    time.sleep(0.5)
+
+
