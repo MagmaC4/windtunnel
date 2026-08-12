05af76c0ea9e25c0eedcddf30b195f87b3038cad
MagmaC4 <magmac495@gmail.com>
2026-08-12 15:07:49 -0500

    use quadratic formula for speed sensor

--- microcontrollers/air_speed_sensor.py
+++ microcontrollers/air_speed_sensor.py
@@ -14,7 +14,16 @@ pcf_in_0 = AnalogIn(pcf, PCF.A0)
 while True:
     raw_value = pcf_in_0.value
     scaled_value = (raw_value / 65535) * pcf_in_0.reference_voltage
-    air_speed = 18.9 * (scaled_value ** 0.484)
+
+    a = 2.78 * (10 ** -3)
+    b = 1.27 * (10 ** -3)
+    c = -7.95 * (10 ** -3) - scaled_value
+
+    # use quadratic formula
+
+
+    air_speed = (-b + (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
+
     print("Pin 0: %0.5fV" % (scaled_value))
     print("Air Speed: %0.5fm/s" % (air_speed))
     print("")
