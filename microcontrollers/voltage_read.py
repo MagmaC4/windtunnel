81e1652db12d9f2896334795d3f091703bf85640
MagmaC4 <magmac495@gmail.com>
2026-08-12 14:09:13 -0500

    Add voltage read program

---
+++ microcontrollers/voltage_read.py
@@ -0,0 +1,20 @@
+import time
+import board
+
+import adafruit_pcf8591.pcf8591 as PCF
+from adafruit_pcf8591.analog_in import AnalogIn
+from adafruit_pcf8591.analog_out import AnalogOut
+
+i2c = board.I2C()
+pcf = PCF.PCF8591(i2c)
+  
+pcf_in_0 = AnalogIn(pcf, PCF.A0)
+
+
+while True:
+	raw_value = pcf_in_0.value
+	scaled_value = (raw_value / 65535) * pcf_in_0.reference_voltage
+	print("Pin 0: %0.5fV" % (scaled_value))
+	print("")
+	time.sleep(0.5)
+
