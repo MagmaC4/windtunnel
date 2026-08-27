6e700134027815cac2e989050a46d66b7690fd47
MagmaC4 <magmac495@gmail.com>
2026-08-27 14:35:38 -0500

    put website under maintenance

---
+++ frontend/app/maintenance/page.tsx
@@ -0,0 +1,14 @@
+export default function Maintenance(){
+    return (
+        <div className="flex flex-col items-center justify-center min-h-dvh">
+
+            <h1 className="text-5xl font-bold">
+            Under Maintenance
+            </h1>
+            <h1 className="text-xl">
+                I'm working on something!
+            </h1>
+        </div>
+
+    )
+}
