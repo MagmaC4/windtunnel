c8ca47b12433a66d4708e78b70fb91d8c507cf3d
MagmaC4 <magmac495@gmail.com>
2026-08-28 12:39:52 -0500

    Add a maintenance header

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from '@/components/Footer';
+import MaintenanceHeader from '@/components/MaintenanceHeader';
 
 // Layout applies to all pages
 // Useful for consistency
@@ -34,6 +35,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
           <body className="min-h-screen flex flex-col">
+            <MaintenanceHeader/> {/* Display Maintenance Notification */}
             <main className="flex-1">
                 {children}
             </main>
---
+++ frontend/components/MaintenanceHeader.tsx
@@ -0,0 +1,7 @@
+export default function MaintenanceHeader(){
+    return (
+        <div className="text-center m-3">
+            Sorry, the site is under maintenance right now! Information may not be accurate.
+        </div>
+    )
+}
