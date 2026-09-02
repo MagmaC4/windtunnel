5d9c1771752e81ecc5ea9a173fb3f184eb1f004a
MagmaC4 <magmac495@gmail.com>
2026-09-02 14:25:24 -0500

    tweak footer text layout so its more pleasant on the eyes

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -14,31 +14,38 @@ export default function Footer(){
 
     return (
         <footer className="border-t bg-footer border-footer-border text-footer-text text-sm py-8 mt-auto transition-colors">
-            <div className="max-w-5xl mx-auto px-4 flex flex-col items-center gap-4">
-                <nav className="flex flex-wrap justify-center gap-2 md:gap-6">
-                    <Link href="/dashboard/open-return" className="flex items-center gap-2 hover:text-footer-text/60 transition-colors">
-                        <TbArrowsRight /> Open Return
-                    </Link>
-
-                    <span className="">|</span>
-
-                    <Link href="/dashboard/closed-return" className="flex items-center gap-2 hover:text-footer-text/60 transition-colors">
-                        <TbChartArrows /> Closed Return
-                    </Link>
-
-                    <span className="text-sm">|</span>
-
-                    <Link href="/contact" className="flex items-center gap-2 hover:text-footer-text/60">
-                        <MdContactEmergency/> Contact
-                    </Link>
-                </nav>
-                <ThemeSwitcher/>
-
-                {/* footer department text */}
+            <div className="max-w-5xl mx-auto px-4 flex flex-col md:flex-row items-center gap-4 justify-between">
+                {/* Department info text */}
                 <div className="">
                     <span className="text-sm"> 430 Akerman Hall, 110 Union St SE, Minneapolis, MN 55455 </span>
-                    <a className="flex gap-2 items-center" href={"https://cse.umn.edu/aem"}> Department of Aerospace Engineering and Mechanics <FaExternalLinkAlt /> </a>
+                    <a className="flex gap-2 items-center" href={"https://cse.umn.edu/aem"}> Department of Aerospace Engineering and Mechanics
+                    <FaExternalLinkAlt size={10}/>
+                    </a>
                 </div>
+
+                {/* Navigation and Theme Switcher*/}
+                <div className="flex flex-col items-center gap-2 justify-between">
+                    <nav className="flex flex-wrap justify-center gap-2 ">
+                        <Link href="/dashboard/open-return" className="flex items-center gap-2 hover:text-footer-text/60 transition-colors">
+                            <TbArrowsRight /> Open Return
+                        </Link>
+
+                        <span className="">|</span>
+
+                        <Link href="/dashboard/closed-return" className="flex items-center gap-2 hover:text-footer-text/60 transition-colors">
+                            <TbChartArrows /> Closed Return
+                        </Link>
+
+                        <span className="text-sm">|</span>
+
+                        <Link href="/contact" className="flex items-center gap-2 hover:text-footer-text/60">
+                            <MdContactEmergency/> Contact
+                        </Link>
+                    </nav>
+                    <ThemeSwitcher/>
+                </div>
+
+
             </div>
         </footer>
     )
