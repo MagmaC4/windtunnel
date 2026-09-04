08c9eaf3df1b7d15d4eb0ecef7109f21f1d03b0e
MagmaC4 <magmac495@gmail.com>
2026-09-04 15:29:52 -0500

    Add blank template file for quickly adding new sensors

---
+++ microcontrollers/template.py
@@ -0,0 +1,91 @@
+# template.py
+# use this file as a template for building a new sensor
+
+import psycopg2                 # PostgreSQL
+from psycopg2 import sql        # PostgreSQL
+import time                     # sleep
+import sys                      # exit early
+import os                       # .env
+from dotenv import load_dotenv  # .env
+# add more imports here
+
+# ==============================================================================
+# Helper Functions
+
+# TODO: finish this function
+def gather_data():
+    return 0
+
+def insert_db():
+    data = gather_data()
+    # Declare which wind tunnel table to insert into (depends on .env file)
+    is_closed = os.getenv("DB_TABLE") == "closed"
+    if is_closed:
+        TABLE_NAME = "closed_thermometer"
+    else:
+        TABLE_NAME = "open_thermometer"
+
+    # TODO: replace example_column  with actual column name
+    # Insert air speed into database
+    sql_insert = psycopg2.sql.SQL("INSERT INTO {table} (example_column) VALUES (%s)").format(
+        table=psycopg2.sql.Identifier(TABLE_NAME)
+    )
+
+    # TODO: replace data if you renamed the variable
+    print(f"Inserting data: {data} readings into table: {TABLE_NAME}...")
+    cursor.execute(sql_insert, (data,)) # execute insert command
+    connection.commit() # save changes to database
+
+
+# ==============================================================================
+# Setup SQL connection
+try:
+    # Load .env file as os.getenv
+    if not load_dotenv():
+        raise Exception("Failed to load .env file")
+
+    # Connect to existing database
+    print("Connecting to PostgreSQL database...")
+    connection = psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT")
+    )
+
+    # Create a cursor to perform database operations
+    cursor = connection.cursor()
+
+except Exception as error:
+    print("Error while connecting to PostgreSQL", error)
+    sys.exit(1)  # stop the program, error code 1
+
+# ==============================================================================
+# Setup microcontroller
+# TODO: add your microcontroller code
+
+
+# ==============================================================================
+# Main Loop
+try:
+    while True:
+        try:
+            insert_db()
+            time.sleep(1)
+        except Exception as error:
+            print("Error while reading from sensor or inserting to database: ", error)
+            connection.rollback()
+
+except KeyboardInterrupt:
+    print("Stopped by user")
+
+# Close out all connections
+finally:
+    if cursor:
+        cursor.close()
+    if connection:
+        connection.close()
+        print("PostgreSQL connection closed")
+
+
